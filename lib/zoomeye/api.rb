a70d5feb3202f4cd33dea85ad2cb5091cf385efe
Manabu Niseki <manabu.niseki@gmail.com>
2021-04-24 11:23:58 +0900

    fix: use API-KEY for auth

--- lib/zoomeye/api.rb
+++ lib/zoomeye/api.rb
@@ -2,34 +2,21 @@
 
 module ZoomEye
   class API
-    def initialize(username: ENV["ZOOMEYE_USERNAME"], password: ENV["ZOOMEYE_PASSWORD"])
-      @username = username
-      raise ArgumentError, "No usernamme has been found or provided!" unless @username
-
-      @password = password
-      raise ArgumentError, "No password has been found or provided!" unless @password
-    end
-
-    def user
-      @user ||= Clients::User.new(username: @username, password: @password)
+    def initialize(api_key: ENV["ZOOMEYE_API_KEY"])
+      @api_key = api_key
+      raise ArgumentError, "No API key has been found or provided!" unless @api_key
     end
 
     def web
-      @web ||= Clients::Web.new(access_token: access_token)
+      @web ||= Clients::Web.new(@api_key)
     end
 
     def host
-      @host ||= Clients::Host.new(access_token: access_token)
+      @host ||= Clients::Host.new(@api_key)
     end
 
     def resource_info
-      @resource_info ||= Clients::ResourceInfo.new(access_token: access_token)
-    end
-
-    private
-
-    def access_token
-      @access_token ||= user.access_token
+      @resource_info ||= Clients::ResourceInfo.new(@api_key)
     end
   end
 end
